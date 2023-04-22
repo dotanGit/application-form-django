5f05d55df315d28766ab424d6c4fea043d8ebf2b
dotan <frenkeldotan1998@gmail.com>
2023-04-22 18:23:21 +0300

    Storing the data into sqlite data base.

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import Form
 
 def index(request):
     if request.method == "POST":
@@ -10,4 +11,6 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date,occupation=occupation)
     return render(request,"index.html")

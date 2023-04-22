ed83ff87e89ec988a07f85fa184af119d1518ef3
dotan <frenkeldotan1998@gmail.com>
2023-04-22 18:25:55 +0300

    View submission message successfully

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
+from django.contrib import messages
 
 def index(request):
     if request.method == "POST":
@@ -13,4 +14,5 @@ def index(request):
             occupation = form.cleaned_data["occupation"]
 
             Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date,occupation=occupation)
+            messages.success(request, "Form submitted successfully")
     return render(request,"index.html")
